ef9d5ea3bd5f5eaaa2f5bd8a313a55c4e2ef7e21
My Siira <my_siira@hotmail.com>
2021-01-04 18:49:12 +0100

    Created separate nagivation component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
 import './assets/App.scss';
-import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap'
-import logo from './assets/logo.svg'
+import { Container } from 'react-bootstrap';
+import NavigationBar from './components/NavigationBar';
 
 const App = () => {
 	return (
 		<>
-			<Navbar bg="dark" variant="dark">
-				<Container>
-					<Navbar.Brand href="/">
-						<img
-							alt="A photoalbum"
-							src={logo}
-							width="30"
-							height="30"
-							className="d-inline-block align-top"
-						/>{' '}
-						My Favourite Photos
-					</Navbar.Brand>
-
-					<Navbar.Toggle aria-controls="basic-navbar-nav" />
-					<Navbar.Collapse id="basic-navbar-nav">
-						<Nav className="ml-auto">
-							<Nav.Link href="/albums">Mina album</Nav.Link>
-							<NavDropdown title="Användare" id="basic-nav-dropdown">
-								<NavDropdown.Item href="/my-profile">Min profil</NavDropdown.Item>
-								<NavDropdown.Divider />
-								<NavDropdown.Item href="/logout">Logga ut</NavDropdown.Item>
-							</NavDropdown>
-						</Nav>
-					</Navbar.Collapse>
-				</Container>
-			</Navbar>
+			<NavigationBar/>
 
 			<Container>
 				Startsida
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,35 @@
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+import logo from '../assets/logo.svg'
+
+const NavigationBar = () => {
+    return (  
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="/">
+                    <img
+                        alt="A photoalbum"
+                        src={logo}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />{' '}
+                    My Favourite Photos
+                </Navbar.Brand>
+
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link href="/albums">Mina album</Nav.Link>
+                        <NavDropdown title="Användare" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/my-profile">Min profil</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="/logout">Logga ut</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+		</Navbar>
+    );
+}
+ 
+export default NavigationBar;
