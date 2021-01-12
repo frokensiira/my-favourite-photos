af2a2a60f0233b675f8870ebbb5f804124c8766d
My Siira <my_siira@hotmail.com>
2021-01-12 14:06:33 +0100

    Functionality to update album with photos and title done

--- src/components/EditAlbum.js
+++ src/components/EditAlbum.js
@@ -1,19 +1,24 @@
 import { Alert, Button, Card, Col, Form, Row } from 'react-bootstrap';
 import { FadeLoader } from 'react-spinners';
+import { useAuth } from '../contexts/AuthContext';
+import { db, storage } from '../firebase';
 import { useDropzone } from 'react-dropzone';
 import { useState, useCallback } from 'react';
-import { useLocation } from 'react-router-dom';
+import { Navigate, useLocation, useNavigate, useParams } from 'react-router-dom';
 import { SRLWrapper } from 'simple-react-lightbox';
 import Photo from './Photo';
 
+
 const EditAlbum = () => {
     const { state } = useLocation();
-    console.log('this is photos', state.photos);
-    console.log('this is albumTitle', state.albumTitle);
-
+    const { albumId } = useParams();
     const [uploadedFiles, setUploadedFiles] = useState([]);
-    
+    const [loading, setLoading] = useState(false);
     const [newAlbumTitle, setNewAlbumTitle] = useState(state.albumTitle);
+    const { currentUser } = useAuth();
+    const [error, setError ] = useState(null);
+    const navigate = useNavigate();
+    console.log('this is albumId', albumId);
 
     const handleCheckBox = (e) => {
 /*         const [image] = photos.filter(photo =>  (photo.id === e.target.id));
@@ -30,11 +35,63 @@ const EditAlbum = () => {
         setNewAlbumTitle(e.target.value);
     }
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    const handleSaveChanges = async (e) => {
 
-        if(uploadedFiles.length === 0){
-            return;
+        if(newAlbumTitle !== state.albumTitle) {
+            try {
+                console.log('inside try');
+                await db.collection('albums').doc(albumId).update({
+                    albumTitle: newAlbumTitle
+                });
+    
+                Promise.all(state.photos.map(photo => {
+                    db.collection('photos').doc(photo.id).update({
+                        albumTitle: newAlbumTitle
+                    });
+                }))
+
+                navigate(`/albums/${albumId}`);
+    
+            } catch (error) {
+                console.log(error.message);
+            }
+        }
+            
+        if(uploadedFiles.length !==0) {
+
+            console.log('I have a file to upload');
+
+            const albumRef = storage.ref(`${newAlbumTitle}`);
+
+            const promises = uploadedFiles.map(async uploadedFile => {
+                setLoading(true);
+    
+                try {
+    
+                    const fileRef = albumRef.child(`/${uploadedFile.name}`);
+                    const snapshot = await fileRef.put(uploadedFile);    
+                    
+                    const url = await snapshot.ref.getDownloadURL();
+    
+                    const photo = {
+                        name: uploadedFile.name,
+                        albumTitle: newAlbumTitle,
+                        path: snapshot.ref.fullPath,
+                        owner: currentUser.uid,
+                        fileUrl: url, 
+                        type: uploadedFile.type,
+                        size: uploadedFile.size
+                    }
+
+                    await db.collection('photos').add(photo);
+                    navigate(`/albums/${albumId}`);
+    
+                } catch(error) {
+                    setError(error.message);
+                }
+    
+            })
+    
         }
         
     }
@@ -55,7 +112,7 @@ const EditAlbum = () => {
     });
 
     return (  
-        <div className="text-center">
+        <div>
             <h1 className="text-center my-4">Redigera album</h1>
 
             <Row className="my-4">
@@ -75,12 +132,26 @@ const EditAlbum = () => {
                                             : (<p>Give me some filez</p>) 
                                     }      
                             </div>
+
+                            
                         </Form.Group>
+
+                        {
+                                uploadedFiles && uploadedFiles.length !== 0 && (
+                                    <div className="accepted-files mt-2">
+                                        <ul className="list-unstyled">
+                                            {uploadedFiles.map(file => (
+                                                <li key={file.name}>
+                                                    {file.name}
+                                                </li>
+                                            ))}
+                                        </ul>   
+                                    </div>
+                                )
+                            } 
                     </Form>
                 </Col>
             </Row>
-
-            
 
             <SRLWrapper>
                 <Row className="mb-5">
@@ -90,125 +161,13 @@ const EditAlbum = () => {
                 </Row>
             </SRLWrapper>
 
+
+            <div className="text-center">
+                <Button className="px-5" onClick={handleSaveChanges}>Spara</Button>
+            </div>
+                                
         </div>
-/*         <div className="text-center">
 
-        {
-            !validUser && !loading
-                ? (<p>You don't have access to this album</p>) 
-                : (
-                    <>
-                        <h1 className="text-center">{albumTitle}</h1>
-
-                        <Button className="my-3" onClick={handleGenerateLink} variant="primary">Generera kundlänk</Button>
-                        {
-                            customerLink && 
-                            (
-                                <div className="my-4">
-                                    <a href={customerLink}>{customerLink}</a>
-                                </div>
-                                
-                            )
-                        }
-
-                        {
-                            loading
-                                ? (<div className="d-flex justify-content-center my-5"><FadeLoader color={'#576675'} size={50}/></div>)
-                                
-                                : (
-                                    <SRLWrapper>
-                                        <Row className="mb-5">
-                                            {photos.map(photo => (
-                                                <Photo photo={photo} key={photo.id} handleCheckBox={handleCheckBox}/>
-                                            ))}
-                                        </Row>
-                                    </SRLWrapper>
-                                )
-                        }
-
-                    
-                        <Row>
-                            <Col md={{ span: 6, offset: 3}}>
-                                <Form>
-                                    <Form.Group>
-                                        <Form.Label>Ange albumets titel:</Form.Label>
-                                        <Form.Control type="name" placeholder="Titel" value={newAlbumTitle} onChange={handleInputChange}/>
-                                    </Form.Group>
-                                    <Button disabled={pickedPhotos.length === 0} onClick={handleSubmit}>Skapa nytt album</Button>
-                                </Form>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col md={{ span: 6, offset: 3}}>
-                                <Form>
-                                    <Link to={`/albums/edit-album/${albumId}`} className="btn btn-secondary my-4" variant="secondary" >Redigera album</Link>
-                                </Form>
-                            </Col>
-                        </Row>
-                    </>
-            )
-        }
-
-        
-            
-    </div>
-        <div>
-
-            {
-                loading && (<div className="d-flex justify-content-center my-5"><FadeLoader color={'#576675'} size={50}/></div>)
-            }
-            
-
-           <Row>
-                <Col md={{ span: 6, offset: 3}}>
-                    <Card>
-                        <Card.Body>
-                            <Card.Title>
-                                Skapa album
-                            </Card.Title>
-
-                            {error && (<Alert variant="danger">{error}</Alert>)}
-
-                            <Form onSubmit={handleSubmit}>
-                                <Form.Group id="title">
-                                    <Form.Label>Albumtitel</Form.Label>
-                                    <Form.Control type="title" value={albumTitle} required onChange={handleChangeTitle}/>
-
-
-                                    <div {...getRootProps()} id="dropzone-wrapper" className={`px-4 py-4 my-3 ${isDragAccept ? `drag-accept` : ``} ${isDragReject ? `drag-reject` : ``}`}>
-                                        <input {...getInputProps()} />
-                                            {
-                                                isDragActive 
-                                                    ? isDragAccept 
-                                                        ? (<p>Just drop it already</p>) 
-                                                        : (<p>Sorry, not the right file type</p>)
-                                                    : (<p>Give me some filez</p>) 
-                                            }      
-                                    </div>
-                                    
-                                </Form.Group>
-
-                                {
-                                    uploadedFiles && uploadedFiles.length !== 0 && (
-                                        <div className="accepted-files mt-2">
-                                            <ul className="list-unstyled">
-                                                {uploadedFiles.map(file => (
-                                                    <li key={file.name}>
-                                                        {file.name}
-                                                    </li>
-                                                ))}
-                                            </ul>   
-                                        </div>
-                                    )
-                                } 
-
-                                <Button disabled={loading} type="submit">Skapa album</Button>
-                            </Form>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </div> */
     );
 }
  
