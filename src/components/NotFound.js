c0e45cb74c37d147a23772784d288ef9104d7953
My Siira <my_siira@hotmail.com>
2021-01-13 18:50:55 +0100

    Updated NotFound component with a little styling

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,8 +1,17 @@
+import '../assets/404.css';
+import { Link } from 'react-router-dom';
+import BodyBackgroundColor from 'react-body-backgroundcolor';
+
 const NotFound = () => {
-    return (  
-        <div>
-            Not Found component
-        </div>
+    return ( 
+        <BodyBackgroundColor backgroundColor='#95c2de'>
+        <div class="mainbox">
+    <div class="err">4</div>
+    <i class="far fa-question-circle fa-spin"></i>
+    <div class="err2">4</div>
+    <div class="msg">Maybe this page moved? Got deleted? Is hiding out in quarantine? Never existed in the first place?<p>Let's go <Link to={'/'}>home</Link> and try from there.</p></div>
+      </div>
+      </BodyBackgroundColor>
     );
 }
  
