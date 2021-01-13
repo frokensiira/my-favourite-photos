6d29b3e4bef2f336b5e36e29fcb7c881904227af
My Siira <my_siira@hotmail.com>
2021-01-13 16:53:03 +0100

    Added test for customer album component

---
+++ src/components/CustomerAlbum.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render, act, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { BrowserRouter } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+
+test('renders albums heading', async () => {
+    const history = createMemoryHistory();
+    window.history.pushState({}, '', ':ownerId/review/:albumId')
+
+    await act(async () => {
+        render(
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        )
+    })
+
+    expect(screen.getByRole('button', { name: 'Förhandsgranska' })).toBeInTheDocument();
+    
+})
